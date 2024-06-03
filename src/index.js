6a14c43bd6c0efea71d4a28a59e6cb3d53585f97
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-06-03 09:53:03 -0300

    feat: create register user

--- src/index.js
+++ src/index.js
@@ -13,6 +13,19 @@ const UserSchema = new mongoose.Schema({
 const User = mongoose.model('User', UserSchema)
 
 
+
+app.post('/register', async (req, res) => {
+  // <TO-DO Validade register>
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email
+  })
+
+  await user.save()
+  return res.send(user)
+})
+
 app.listen(port, async () => {
   await mongoose.connect('mongodb+srv://luizgmelo64:<password>@login-api.t0dohpy.mongodb.net/?retryWrites=true&w=majority&appName=login-api')
   console.log(`App running and listen at port ${port}`)
