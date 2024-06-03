f7b26b36ae31f1a1366ff4aa171c57bc99f5961e
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-06-03 09:53:27 -0300

    feat: create login with jwt token authentication

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
 const app = express()
 app.use(express.json())
 const port = 3000
+const mySecret = "mySecret"
 
 const UserSchema = new mongoose.Schema({
   username: String,
@@ -13,6 +15,44 @@ const UserSchema = new mongoose.Schema({
 const User = mongoose.model('User', UserSchema)
 
 
+app.get('/hello', (req, res) => {
+  let token = req.headers['authorization']
+  if (token) {
+    token = token.replace(/^Bearer\s+/, "")
+    jwt.verify(token, mySecret, (err, decoded) => {
+      if (err) {
+        return res.sendStatus(403)
+      } else {
+        return res.send(`Hello ${decoded['username']}`)
+      }
+    })
+  } else {
+    return res.sendStatus(403)
+  }
+
+})
+
+app.post('/login', async (req, res) => {
+  // <TO-DO Validade login>
+  const userPayload = new User({
+    email: req.body.email,
+    password: req.body.password
+  })
+
+  const query = User.where({email: userPayload.email})
+  const user = await query.findOne()
+  if (user != null) {
+    if (user.password == userPayload.password) {
+      const token = jwt.sign({email: user.email, username: user.username}, mySecret)
+      res.send(token)
+    } else {
+      res.send('Ops... Wrong Email or Password!')
+    }
+  } else {
+    res.send('Ops... Wrong Email or Password!')
+  }
+
+})
 
 app.post('/register', async (req, res) => {
   // <TO-DO Validade register>
